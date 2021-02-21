28303dc22b832011279a2eb11041a93ac82cbfe6
linusschrab <linus.schrab@gmail.com>
2021-02-21 22:08:38 +0100

    Initial commit

---
+++ nest_midi0.lua
@@ -0,0 +1,192 @@
+return { 
+    grid = { 
+        pattern_group = { 
+            control_preset = { 
+                { 
+                    state = { 
+                        { 
+                            hzlag = { 
+                                value = 0.5,
+                                output = { },
+                                input = { },
+                            },
+                            timbre = { 
+                                value = 0.66666666666667,
+                                output = { },
+                                input = { },
+                            },
+                            cut = { 
+                                value = 0.16666666666667,
+                                output = { },
+                                input = { },
+                            },
+                            noise = { 
+                                value = 0.5,
+                                output = { },
+                                input = { },
+                            },
+                            sustain = { 
+                                value = 0.66666666666667,
+                                output = { },
+                                input = { },
+                            },
+                            attack = { 
+                                value = 0.16666666666667,
+                                output = { },
+                                input = { },
+                            },
+                            shape = { 
+                                value = 0.33333333333333,
+                                output = { },
+                                input = { },
+                            },
+                            release = { 
+                                value = 0.33333333333333,
+                                output = { },
+                                input = { },
+                            },
+                        },
+},
+                },
+
+                output = { },
+                value = 1,
+                input = { },
+            },
+        },
+        pattern = { 
+            { 
+                play = 0,
+                step = 0,
+                count = 0,
+                event = { },
+                time = { },
+                time_factor = 1,
+            },
+            { 
+                play = 0,
+                step = 0,
+                count = 0,
+                event = { },
+                time = { },
+                time_factor = 1,
+            },
+            { 
+                play = 0,
+                step = 0,
+                count = 0,
+                event = { },
+                time = { },
+                time_factor = 1,
+            },
+            { 
+                play = 0,
+                step = 0,
+                count = 0,
+                event = { },
+                time = { },
+                time_factor = 1,
+            },
+            { 
+                play = 0,
+                step = 0,
+                count = 0,
+                event = { },
+                time = { },
+                time_factor = 1,
+            },
+            { 
+                play = 0,
+                step = 0,
+                count = 0,
+                event = { },
+                time = { },
+                time_factor = 1,
+            },
+            { 
+                play = 0,
+                step = 0,
+                count = 0,
+                event = { },
+                time = { },
+                time_factor = 1,
+            },
+            { 
+                play = 0,
+                step = 0,
+                count = 0,
+                event = { },
+                time = { },
+                time_factor = 1,
+            },
+
+            output = { },
+            value = { 0, 0, 0, 0, 0, 0, 0, 0, },
+            input = { },
+        },
+        controls = { 
+            hzlag = { 
+                output = { },
+                value = 0.5,
+                input = { },
+            },
+            cut = { 
+                output = { },
+                value = 0.16666666666667,
+                input = { },
+            },
+            release = { 
+                output = { },
+                value = 0.33333333333333,
+                input = { },
+            },
+            noise = { 
+                output = { },
+                value = 0.5,
+                input = { },
+            },
+            sustain = { 
+                output = { },
+                value = 0.66666666666667,
+                input = { },
+            },
+            timbre = { 
+                output = { },
+                value = 0.66666666666667,
+                input = { },
+            },
+            shape = { 
+                output = { },
+                value = 0.33333333333333,
+                input = { },
+            },
+            attack = { 
+                output = { },
+                value = 0.16666666666667,
+                input = { },
+            },
+        },
+    },
+    screen = { 
+        rate = { 
+            output = { },
+            value = 0.5,
+            input = { },
+        },
+        delay = { 
+            output = { },
+            value = 0.5,
+            input = { },
+        },
+        reverse = { 
+            output = { },
+            value = 0,
+            input = { },
+        },
+        feedback = { 
+            output = { },
+            value = 0.75,
+            input = { },
+        },
+    },
+}
